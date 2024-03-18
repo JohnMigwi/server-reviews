58ee7a374d8a3e3d66a3323099e73969be160850
JohnMigwi <johnmigwi101@gmail.com>
2024-03-18 21:48:27 +0300

    pipelines

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
-import express from 'express'
-import cors from 'cors'
-import movies from './api/movies.route.js'
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use("/api/v1/movies", movies)
-app.use('*', (req,res)=>{
-res.status(404).json({error: "not found"})
-})
-export default app
+import express from 'express';
+import cors from 'cors';
+import moviesRouter from './api/movies.route.js';
+import mongoose from 'mongoose';
+
+// Connect to MongoDB using createConnection
+const connection = mongoose.createConnection(process.env.MOVIEREVIEW_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Event listeners to handle connection events
+connection.on('connected', () => {
+    console.log("Connected to MongoDB");
+});
+
+connection.on('error', (err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/movies", moviesRouter);
+
+app.use('*', (req,res) => {
+    res.status(404).json({error:"not found"});
+});
+
+export default app;
