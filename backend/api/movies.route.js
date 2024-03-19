45936663eb8fd32e05167df77b3b1b27773dde88
JohnMigwi <johnmigwi101@gmail.com>
2024-03-19 20:18:08 +0300

    connected to server successfully

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,18 +1,4 @@
-import express from "express";
-import Movie from "../models/movie.model.js";
-
-const router = express.Router();
-
-// Get all movies
-router.get('/', async (req, res) => {
-    try {
-        const movies = await Movie.find();
-        res.json(movies);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-});
-
-// Define other routes for CRUD operations
-
-export default router;
+import express from 'express'
+const router = express.Router() // get access to express router
+router.route('/').get((req,res) => res.send('hello world'))
+export default router
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
-import app from "./server.js";
-import dotenv from 'dotenv';
-import mongodb from 'mongodb';
-
-
+import app from './server.js'
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+async function main(){
 dotenv.config()
-async function main() {
-    const port = process.env.PORT || 8000;
-
-    app.listen(port, () => {
-        console.log('Server is running on port:', port);
-    });
+const client = new mongodb.MongoClient(
+'mongodb+srv://john:john@cluster0.kktnovp.mongodb.net/'
+);
+const port = process.env.PORT || 8000
+try {
+// Connect to the MongoDB cluster
+await client.connect()
+app.listen(port, () =>{
+console.log('server is running on port:'+port);
+})
+} catch (e) {
+console.error(e);
+process.exit(1)
 }
-
-console.log("The port is", process.env.URI)
-
+}
 main().catch(console.error);
